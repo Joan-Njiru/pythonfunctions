23500fce4d71a9009cc86bd5909c0348ea12eff1
Joan Njiru <joanwanjirunjiru@gmail.com>
2023-03-31 15:34:17 +0300

    pythonfunctions

---
+++ functions/math.py
@@ -0,0 +1,15 @@
+def addition(a,b):
+    answer= a+b
+    return answer
+
+def subtract(a,b):
+    answer= a-b
+    return answer
+
+def multiply(a,b):
+    answer= a*b
+    return answer
+
+def remainder(a,b):
+    answer= a%b
+    return answer
